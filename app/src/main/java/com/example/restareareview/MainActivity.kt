673e969a49fd0e84941482358a5a0b891db9a362
Sangwon <jackoss@naver.com>
2020-06-05 20:43:02 +0900

    chore: set actionbar

--- app/src/main/java/com/example/restareareview/MainActivity.kt
+++ app/src/main/java/com/example/restareareview/MainActivity.kt
@@ -1,18 +1,21 @@
 package com.example.restareareview
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageButton
 import android.widget.RelativeLayout
+import androidx.appcompat.app.ActionBar
 import com.example.restareareview.Adapter.MainFragmentStatePagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+//    lateinit var toolbar: androidx.appcompat.widget.Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setToolBar()
         init()
         configureBottomNavigation()
     }
@@ -20,7 +23,28 @@ class MainActivity : AppCompatActivity() {
     private fun init() {
 
     }
+//    private fun makeToolBar(){
+//        toolbar=findViewById(R.id.myToolBar)
+//        setSupportActionBar(toolbar)
+//    }
+    private fun setToolBar(){
+        supportActionBar?.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar?.setDisplayShowCustomEnabled(true)
+        supportActionBar?.setCustomView(R.layout.custom_action_bar_layout)
 
+        val view = supportActionBar!!.customView
+        val imageButton1 = view.findViewById<ImageButton>(R.id.back)
+        imageButton1.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val imageButton2 = view.findViewById<ImageButton>(R.id.mail)
+        imageButton2.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
     private fun configureBottomNavigation(){
         main_frag_paper.adapter = MainFragmentStatePagerAdapter(supportFragmentManager, 3)
         main_bottom_menu.setupWithViewPager(main_frag_paper)
