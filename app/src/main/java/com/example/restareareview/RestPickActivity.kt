1ead49fbbf03baf76a65c73c847aef07074198ea
Sangwon <jackoss@naver.com>
2020-06-15 17:53:05 +0900

    second fragment

---
+++ app/src/main/java/com/example/restareareview/RestPickActivity.kt
@@ -0,0 +1,11 @@
+package com.example.restareareview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RestPickActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rest_pick)
+    }
+}
